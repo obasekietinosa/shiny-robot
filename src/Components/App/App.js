9074a99ef5853a1c6a0e452a9e78d273f5ceb487
Etin <etinosaobaseki@gmail.com>
2020-03-14 23:23:58 +0100

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+
+    </Fragment>
+  );
+}
+
+export default App;
